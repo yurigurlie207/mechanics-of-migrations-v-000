c5939064539c5572e7045c5f2297fd11a392ab44
Adrienne Yao <adrienne.yao@icloud.com>
2019-07-25 01:20:40 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -16,18 +16,3 @@ ActiveRecord::Base.establish_connection(
 )
 
 require_relative "../artist.rb"
-
-
-
-
-sql = <<-SQL
-  CREATE TABLE IF NOT EXISTS artists (
-  id INTEGER PRIMARY KEY,
-  name TEXT,
-  genre TEXT,
-  age INTEGER,
-  hometown TEXT
-  )
-SQL
-
-ActiveRecord::Base.connection.execute(sql)
